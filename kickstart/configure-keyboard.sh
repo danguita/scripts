910496be35b6fb6a2048e9e8f643c06e4f24de9f
David Anguita <david@davidanguita.name>
2020-11-08 08:38:32 +0100

    (configure-keyboard) Add link to the Arch Wiki for reference

--- kickstart/configure-keyboard.sh
+++ kickstart/configure-keyboard.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 #
-# Script to have the `hid_apple` module parameters set on boot.
+# Script to have the `hid_apple` module parameters set on boot. See [1] for
+# reference.
+#
+# [1] https://wiki.archlinux.org/index.php/Apple_Keyboard#Function_keys_do_not_work
 #
 # Run me with:
 #
