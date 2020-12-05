d3be8c8a6c7c84f361a6ce97c1c4c7606e7d4313
David Anguita <david@davidanguita.name>
2020-12-05 12:42:12 +0100

    (install-st) Add standalone script to install st

---
+++ kickstart/install-st.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Script to compile dwm from source.
+#
+# - Requires ./kickstart-void.sh to work.
+#
+# Run me with:
+#
+# $ ./install-st.sh
+
+[ ! -f ./kickstart-void.sh ] && \
+  printf "Error: %s cannot be found\n" "./kickstart-void.sh" && \
+  exit 1
+
+# shellcheck disable=SC1091
+source ./kickstart-void.sh --source-only && install_st
