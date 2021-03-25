59bb86fce850c21fe47b8e6b37d68981cd0ae2c9
David Anguita <david@davidanguita.name>
2021-03-25 07:03:15 +0100

    (install-void) Bump kernel to linux5.11

--- kickstart/install-void.sh
+++ kickstart/install-void.sh
@@ -40,8 +40,8 @@ device=/dev/sda # It typically is `/dev/nvme0n1` in NVMe drives.
 swap_partition_size=2G
 
 # Kernel version the system will boot on.
-# https://github.com/void-linux/void-packages/blob/597174314788beeb318eaf97b9fea6ae19bcff97/srcpkgs/linux/template#L3
-kernel_version=linux5.10
+# https://github.com/void-linux/void-packages/blob/ae0fc94307e716b37198db610aa116bf7fca4986/srcpkgs/linux/template#L3
+kernel_version=linux5.11
 
 # Time zone in `zoneinfo` format.
 time_zone=Europe/Madrid
